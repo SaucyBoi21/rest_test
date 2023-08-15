b897be40b41690355aee048ab208607feeb12b37
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-08-15 11:03:53 -0400

    add api

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 )
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'tshirt',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const
+});
